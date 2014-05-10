eb7a30f58d62dcf61d1445efd7ba84b3de2f4e68
Kevin Moore <github@j832.com>
2014-05-10 13:11:20 -0700

    Added example

---
+++ example/example_server.dart
@@ -0,0 +1,19 @@
+library shelf_static.example;
+
+import 'dart:io';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_static/shelf_static.dart';
+
+void main() {
+  if (!FileSystemEntity.isFileSync('example/example_server.dart')) {
+    throw new StateError('Server expects to be started the '
+        'root of the project.');
+  }
+  var handler = const shelf.Pipeline().addMiddleware(shelf.logRequests())
+      .addHandler(getHandler('example/files'));
+
+  io.serve(handler, 'localhost', 8080).then((server) {
+    print('Serving at http://${server.address.host}:${server.port}');
+  });
+}
