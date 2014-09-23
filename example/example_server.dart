afe2d92cd5a59d7a33b817be3974e88a8a838479
Kevin Moore <github@j832.com>
2014-09-23 12:06:39 -0700

    example: allow opt-out of logging (better for perf testing)

--- example/example_server.dart
+++ example/example_server.dart
@@ -1,20 +1,32 @@
 library shelf_static.example;
 
 import 'dart:io';
+import 'package:args/args.dart';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_static/shelf_static.dart';
 
-void main() {
+void main(List<String> args) {
+  var result = _getParser().parse(args);
+  var logging = result['logging'];
+
   if (!FileSystemEntity.isFileSync('example/example_server.dart')) {
     throw new StateError('Server expects to be started the '
         'root of the project.');
   }
-  var handler = const shelf.Pipeline().addMiddleware(shelf.logRequests())
-      .addHandler(createStaticHandler('example/files',
+  var pipeline = const shelf.Pipeline();
+
+  if (logging) {
+    pipeline = pipeline.addMiddleware(shelf.logRequests());
+  }
+
+  var handler = pipeline.addHandler(createStaticHandler('example/files',
           defaultDocument: 'index.html'));
 
   io.serve(handler, 'localhost', 8080).then((server) {
     print('Serving at http://${server.address.host}:${server.port}');
   });
 }
+
+ArgParser _getParser() => new ArgParser()
+    ..addFlag('logging', abbr: 'l', defaultsTo: true, negatable: true);
