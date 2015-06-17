2f8282a61b8e79132175e6d14a36ac5ad68b48f4
Kevin Moore <kevmoo@google.com>
2015-06-17 16:18:45 -0700

    fix travis builds by skipping coverage
    until https://github.com/dart-lang/scheduled_test/issues/20 is fixed

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Fast fail the script on failures.   
+# Fast fail the script on failures.
 set -e
 
-dart --checked test/test_all.dart
+pub run test
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
-  pub global run dart_coveralls report \
-    --token $COVERALLS_TOKEN \
-    --retry 2 \
-    --exclude-test-files \
-    test/test_all.dart
+  echo "Skipping coverage until https://github.com/dart-lang/scheduled_test/issues/20 is fixed"
+#
+#  pub global run dart_coveralls report \
+#    --exclude-test-files \
+#    test/test_all.dart
 fi
