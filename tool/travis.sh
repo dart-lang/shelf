d639c445f042b08bdbb3ab1bf769eab4f165d7e9
Kevin Moore <kevmoo@google.com>
2015-07-06 15:07:10 -0700

    reenable code coverage

--- tool/travis.sh
+++ tool/travis.sh
@@ -7,9 +7,9 @@ pub run test
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
-  echo "Skipping coverage until https://github.com/dart-lang/scheduled_test/issues/20 is fixed"
-#
-#  pub global run dart_coveralls report \
-#    --exclude-test-files \
-#    test/test_all.dart
+  pub global activate dart_coveralls
+
+  pub global run dart_coveralls report \
+    --exclude-test-files \
+    test/test_all.dart
 fi
