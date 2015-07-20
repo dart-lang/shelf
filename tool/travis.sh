4f69983225782f16a4dc1297c0d14096712215aa
Kevin Moore <kevmoo@google.com>
2015-07-20 15:19:27 -0700

    updated travis integration

--- tool/travis.sh
+++ tool/travis.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Fast fail the script on failures.   
+# Fast fail the script on failures.
 set -e
 
-dart --checked test/test_all.dart
+pub run test
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  pub global activate dart_coveralls
   pub global run dart_coveralls report \
-    --token $COVERALLS_TOKEN \
-    --retry 2 \
     --exclude-test-files \
+    --log-level warning \
     test/test_all.dart
 fi
