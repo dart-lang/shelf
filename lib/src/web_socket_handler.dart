7120b623b401978bf3605b614444e600b63f186a
Natalie Weizenbaum <nweiz@google.com>
2016-01-12 16:44:49 -0800

    Get rid of all the library tags.
    R=kevmoo@google.com
    
    Review URL: https://codereview.chromium.org//1587433002 .

--- lib/src/web_socket_handler.dart
+++ lib/src/web_socket_handler.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-library shelf_web_socket.web_socket_handler;
-
 import 'dart:convert';
 
 import 'package:http_parser/http_parser.dart';
