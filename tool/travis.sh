55067ed3fe2547334190dec621fce88e6ff3707f
Kevin Moore <kevmoo@google.com>
2016-03-03 12:31:18 -0800

    test browsers on Travis-CI
    R=nweiz@google.com
    
    Review URL: https://codereview.chromium.org//1761003002 .

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,7 +3,7 @@
 # Fast fail the script on failures.
 set -e
 
-pub run test
+pub run test -p vm,content-shell,firefox
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
