df794c652ece4ac831b3518319bf6de5d7496126
Kevin Moore <kevmoo@users.noreply.github.com>
2021-04-08 10:05:24 -0700

    pipeline: eliminate null checks (#174)

--- lib/src/pipeline.dart
+++ lib/src/pipeline.dart
@@ -13,30 +13,31 @@ import 'middleware.dart';
 ///         .addMiddleware(cachingMiddleware)
 ///         .addHandler(application);
 class Pipeline {
-  final Pipeline? _parent;
-  final Middleware? _middleware;
-
-  const Pipeline()
-      : _middleware = null,
-        _parent = null;
-
-  Pipeline._(this._middleware, this._parent);
+  const Pipeline();
 
   /// Returns a new [Pipeline] with [middleware] added to the existing set of
   /// [Middleware].
   ///
   /// [middleware] will be the last [Middleware] to process a request and
   /// the first to process a response.
-  Pipeline addMiddleware(Middleware middleware) => Pipeline._(middleware, this);
+  Pipeline addMiddleware(Middleware middleware) =>
+      _Pipeline(middleware, addHandler);
 
   /// Returns a new [Handler] with [handler] as the final processor of a
   /// [Request] if all of the middleware in the pipeline have passed the request
   /// through.
-  Handler addHandler(Handler handler) {
-    if (_middleware == null) return handler;
-    return _parent!.addHandler(_middleware!(handler));
-  }
+  Handler addHandler(Handler handler) => handler;
 
   /// Exposes this pipeline of [Middleware] as a single middleware instance.
   Middleware get middleware => addHandler;
+}
+
+class _Pipeline extends Pipeline {
+  final Middleware _middleware;
+  final Middleware _parent;
+
+  _Pipeline(this._middleware, this._parent);
+
+  @override
+  Handler addHandler(Handler handler) => _parent(_middleware(handler));
 }
